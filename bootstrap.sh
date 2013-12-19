00c48e81c9cbbb774c3d608b8d1fa1da0c8fb97f
Jon Wayne Parrott <jjramone13@gmail.com>
2013-12-19 15:07:47 -0500

    Using new cloud SDK installer

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,49 +6,36 @@ set -e
 
 sudo echo 'Development Environment Bootstrap Script'
 
-echo 'Installing Ubuntu Packages with apt'
+# echo 'Installing Ubuntu Packages with apt'
 
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
-sudo add-apt-repository ppa:chris-lea/node.js
+# sudo add-apt-repository ppa:webupd8team/sublime-text-2
+# sudo add-apt-repository ppa:chris-lea/node.js
 
-sudo apt-get update
-sudo apt-get -yq install build-essential git git-extras wget zip unzip p7zip python-crypto python-httplib2 python-imaging python-lxml python-markupsafe python-oauth python-openssl python-pip python-setuptools python-simplejson phantomjs nodejs sublime-text
+# sudo apt-get update
+# sudo apt-get -yq install curl build-essential git git-extras wget zip unzip p7zip python-crypto python-httplib2 python-imaging python-lxml python-markupsafe python-oauth python-openssl python-pip python-setuptools python-simplejson phantomjs nodejs sublime-text
 
-echo 'Installing Python Packages with pip'
+# echo 'Installing Python Packages with pip'
 
-sudo pip install unittest2 Jinja2 GitPython Sphinx WebOb WebTest pyflakes beautifulsoup4 nose
-sudo pip install --pre ferrisnose
+# sudo pip install unittest2 Jinja2 GitPython Sphinx WebOb WebTest pyflakes beautifulsoup4 nose
+# sudo pip install --pre ferrisnose
 
-echo 'Installing Karma'
+# echo 'Installing Karma'
 
-sudo npm install -g karma
+# sudo npm install -g karma
 
-echo 'Downloading App Engine SDK'
+echo 'Downloading Cloud SDK'
+echo 'About to download and run the Cloud SDK installer. Please be sure do the following:'
+echo ' - Install in your home directory. (enter at the first prompt)'
+echo ' - Either allow Google to collect data or not. (Y/n at second prompt)'
+echo ' - Install the Python and PHP App Engine runtime. (2 at the third prompt) (you can install java later)'
+echo ' - Allow it to update system path (enter at the fourth prompt)'
+echo ' - Use ~/.bashrc (enter at the fifth prompt)'
+echo ' - Enable completion (enter at the sixth prompt)'
+echo 'Press enter when you feel like you are ready to party'
 
-cd /tmp
+read _
 
-LATEST_APPENGINE=$(python -c "from httplib2 import Http; from bs4 import BeautifulSoup; http = Http(); r, c = http.request('http://developers.google.com/appengine/downloads'); soup = BeautifulSoup(c); links = soup.select('a[href*=".zip"]'); print links[0]['href'];")
-
-if [ ! "$LATEST_APPENGINE" ]; then
-    echo "Oh no, couldn't find the latest App Engine. This script may need some adjusting!"
-fi
-
-rm google_appengine_*
-
-wget $LATEST_APPENGINE
-
-echo 'Extracting App Engine SDK'
-unzip google_appengine_*
-
-echo 'Installing App Engine SDK'
-rm -rf ~/bin/google_appengine
-mkdir -p ~/bin
-mv -f google_appengine ~/bin
-
-echo 'Configuring Path'
-echo $'\n# Google App Engine\n\nPATH=\"$HOME/bin/google_appengine:$PATH\"\nexport APPENGINE_SDK_PATH=$HOME/bin/google_appengine' >> ~/.profile
-source ~/.profile
-
+curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
 
 echo 'Downloading Support Scripts (app-server and ferris-tests)'
 
