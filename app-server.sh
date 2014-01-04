c58175e3a278352c5dbcf451392e47dc964d1f25
Jon Wayne Parrott <jjramone13@gmail.com>
2014-01-03 19:12:48 -0500

    Silent cloud sdk install

--- app-server.sh
+++ app-server.sh
@@ -7,4 +7,4 @@ if [ ! -d "$DATA_PATH" ]; then
     mkdir -p $DATA_PATH
 fi
 
-~/bin/google_appengine/dev_appserver.py --storage_path=$DATA_PATH/storage --blobstore_path=$DATA_PATH/blobstore --datastore_path=$DATA_PATH/datastore --search_indexes_path=$DATA_PATH/searchindexes --show_mail_body=yes $@ .
+dev_appserver.py --storage_path=$DATA_PATH/storage --blobstore_path=$DATA_PATH/blobstore --datastore_path=$DATA_PATH/datastore --search_indexes_path=$DATA_PATH/searchindexes --show_mail_body=yes $@ .
--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ echo 'Installing Ubuntu Packages with apt'
 
 # Accept oracle license
 sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get -yq install curl build-essential git git-extras wget zip unzip p7zip python-crypto python-httplib2 python-imaging python-lxml python-markupsafe python-oauth python-openssl python-pip python-setuptools python-simplejson phantomjs nodejs sublime-text-installer oracle-java7-installer
+sudo apt-get -yq install curl wget build-essential git git-extras wget zip unzip p7zip python-crypto python-httplib2 python-imaging python-lxml python-markupsafe python-oauth python-openssl python-pip python-setuptools python-simplejson phantomjs nodejs sublime-text-installer oracle-java7-installer
 
 echo 'Installing Python Packages with pip'
 
@@ -32,18 +32,20 @@ echo 'Installing Node packages (karma, jshint, bower)'
 sudo npm install -g karma jshint bower
 
 echo 'Downloading Cloud SDK'
-echo 'About to download and run the Cloud SDK installer. Please be sure do the following:'
-echo ' - Install in your home directory. (enter at the first prompt)'
-echo ' - Either allow Google to collect data or not. (Y/n at second prompt)'
-echo ' - Install the Python and PHP App Engine runtime. (2 at the third prompt) (you can install java later)'
-echo ' - Allow it to update system path (enter at the fourth prompt)'
-echo ' - Use ~/.bashrc (enter at the fifth prompt)'
-echo ' - Enable completion (enter at the sixth prompt)'
-echo 'Press enter when you feel like you are ready to party'
 
-read _
+cd ~
 
-curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
+# Download
+wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz
+tar xzvpf google-cloud-sdk.tar.gz
+rm google-cloud-sdk.tar.gz
+
+# Silent install
+env CLOUDSDK_REINSTALL_COMPONENTS=pkg-core,pkg-python ./google-cloud-sdk/install.sh
+
+# Configure path
+echo "PATH=\"\$HOME/google-cloud-sdk/bin:\$PATH\"" >> ~/.profile
+echo "export APPENGINE_SDK_PATH=\$HOME/google-cloud-sdk/platform/google_appengine" >> ~/.profile
 
 echo 'Downloading Support Scripts (app-server and ferris-tests)'
 
